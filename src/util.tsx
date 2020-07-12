b347bdc4f65db03922c408d448f313a28aae9884
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-07-12 17:25:23 +0530

    Set forge variables

--- src/util.tsx
+++ src/util.tsx
@@ -1,8 +1,7 @@
 import { BitlyClient } from "bitly";
 import { BitlyLink } from "bitly/dist/types";
 import { stringify } from "querystring";
-const bitly = new BitlyClient("963ddf0ef3436b4477aab0c9d50bacc2bb451782", {});
-console.log(process.env.BITLY_GENERTIC_ACCESS_TOKEN);
+const bitly = new BitlyClient(process.env.BITLY_GENERTIC_ACCESS_TOKEN, {});
 
 export const shortenUrl = async (longUrl: string): Promise<string> => {
   if (longUrl == "") {
