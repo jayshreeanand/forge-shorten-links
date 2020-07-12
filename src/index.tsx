48d1d815ae4853ab9e9cbcceaf8f17a722ae534a
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-07-12 13:46:14 +0530

    Macro for link shortening

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
-import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
+import ForgeUI, {
+  render,
+  useConfig,
+  Fragment,
+  Text,
+  TextField,
+  Select,
+  Option,
+  ConfigForm,
+  Macro,
+  Image,
+} from "@forge/ui";
+
+import { generateShortenedUrl } from "./util";
+
+const defaultConfig = {
+  longUrl: "https://github.com/tanepiper/node-bitly",
+};
+
+const Config = () => {
+  return (
+    <ConfigForm>
+      <TextField
+        label="Input URL"
+        name="longUrl"
+        defaultValue="https://github.com/tanepiper/node-bitly"
+      />
+    </ConfigForm>
+  );
+};
 
 const App = () => {
+  const { longUrl } = useConfig();
+  // const daysLeft = daysTill(time);
+  const shortUrl = generateShortenedUrl(longUrl);
   return (
     <Fragment>
       <Text>Hello world!</Text>
+      <Fragment>
+        <a href={shortUrl} alt={shortUrl}>
+          {shortUrl}
+        </a>
+      </Fragment>
     </Fragment>
   );
 };
 
 export const run = render(
-  <Macro
-    app={<App />}
-  />
+  <Macro app={<App />} config={<Config />} defaultConfig={defaultConfig} />
 );
---
+++ src/util.tsx
@@ -0,0 +1,44 @@
+// import moment from "moment";
+
+// export function daysTill(time) {
+//   var now = moment();
+//   const daysTill = now.to(time, true);
+//   return daysTill === "Invalid date" ? null : daysTill;
+// }
+
+// export function generateDataURI(daysTillText) {
+//   const svg =
+//     '<?xml version="1.0" encoding="UTF-8"?>' +
+//     '<svg width="550px" height="243px" viewBox="0 0 550 243" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
+//     '<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">' +
+//     '<g id="counter">' +
+//     '<rect id="box" stroke="#000000" stroke-width="5" fill="#FFFFFF" x="2.5" y="2.5" width="545" height="238"></rect>' +
+//     '<text id="days-text" fill="#FF0000" font-family="Lato-Bold, Lato" font-size="103" font-weight="bold">' +
+//     '<tspan x="99.4515" y="161">' +
+//     daysTillText +
+//     "</tspan>" +
+//     "</text>" +
+//     "</g>" +
+//     "</g>" +
+//     "</svg>";
+//   const encodedSvg = new Buffer(svg).toString("base64");
+//   const dataUri = `data:image/svg+xml;base64,${encodedSvg}`;
+//   return dataUri;
+// }
+
+import { BitlyClient } from "bitly";
+const bitly = new BitlyClient(process.env.BITLY_GENERTIC_ACCESS_TOKEN, {});
+
+export function generateShortenedUrl(longUrl) {
+  init();
+}
+
+async function init() {
+  let result;
+  try {
+    result = await bitly.shorten("https://github.com/tanepiper/node-bitly");
+  } catch (e) {
+    throw e;
+  }
+  return result;
+}
