e562d1eedbb7e9a05ba9bd12d9d9b749ff2cb512
Jayshree Anandakumar <ping.jayshree@gmail.com>
2020-07-12 18:08:52 +0530

    Fix API access issue

--- src/util.tsx
+++ src/util.tsx
@@ -1,7 +1,12 @@
+import React, { useState, useEffect, Fragment } from "react";
+
 import { BitlyClient } from "bitly";
-import { BitlyLink } from "bitly/dist/types";
-import { stringify } from "querystring";
-const bitly = new BitlyClient(process.env.BITLY_GENERIC_ACCESS_TOKEN, {});
+
+console.log(process.env.BITLY_GENERIC_ACCESS_TOKEN);
+
+var accessToken = process.env.BITLY_GENERIC_ACCESS_TOKEN;
+accessToken = "963ddf0ef3436b4477aab0c9d50bacc2bb451782";
+const bitly = new BitlyClient(accessToken, {});
 
 export const shortenUrl = async (longUrl: string): Promise<string> => {
   if (longUrl == "") {
